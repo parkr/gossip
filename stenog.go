59dcebc0bc908e47bc7f5313382d02b2ced34ddb
Parker Moore <parkrmoore@gmail.com>
2014-05-20 02:24:58 -0400

    sqlx makes this way easier... in theory.

--- stenog.go
+++ stenog.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/go-martini/martini"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"html"
+	"net/http"
 	"time"
 )
 
@@ -16,12 +19,15 @@ const (
 )
 
 type Message struct {
-	Room       string `form:"room" json:"room" binding:"required"`
-	Author     string `form:"author" json:"author" binding:"required"`
-	Message    string `form:"message" json:"message" binding:"required"`
+	Id         int    `json:"id" db:"id"`
+	Room       string `form:"room" json:"room" db:"room" binding:"required"`
+	Author     string `form:"author" json:"author" db:"author" binding:"required"`
+	Message    string `form:"message" json:"message" db:"message" binding:"required"`
 	At         string `form:"time" json:"time" binding:"required"`
 	unexported string `form:"-"` // skip binding of unexported fields
 }
+
+type Messages []*Message
 
 func (msg *Message) String() string {
 	return "<" + msg.Room + " by " + msg.Author + " at " + msg.At + ": " + msg.Message + ">"
@@ -31,6 +37,7 @@ func main() {
 	// Setup Martini
 	m := martini.Classic()
 	m.Get("/", sayHello)
+	m.Get("/messages/latest", fetchLatestMessages)
 	m.Post("/api/messages/log", binding.Bind(Message{}), storeMessage)
 	m.Run()
 }
@@ -43,8 +50,40 @@ func newDb() *sql.DB {
 	return db
 }
 
+func fancyDb() *sqlx.DB {
+	db, err := sqlx.Connect("mysql", "dbname=bar")
+	if err != nil {
+		fmt.Println("CRAP the db couldn't be connected to.")
+	}
+	return db
+}
+
 func sayHello() string {
 	return "Hello, world"
+}
+
+func fetchLatestMessages(req *http.Request) string {
+	limit := req.URL.Query().Get("limit")
+	if limit == "" { // no limit
+		limit = "10"
+	}
+	fmt.Println("limit =", limit)
+
+	db := fancyDb()
+
+	messages := []Message{}
+	err := db.Select(&messages, "SELECT * FROM messages ORDER BY at DESC LIMIT 0,"+limit)
+	fmt.Println(messages)
+
+	if err != nil {
+		json, _ := json.Marshal(messages)
+
+		return "{ok:true,limit:" + limit + ",values:" + string(json) + "}"
+	} else {
+		fmt.Println(messages)
+		fmt.Println(err)
+		return FailedJsonMessage
+	}
 }
 
 func storeMessage(msg Message) string {
