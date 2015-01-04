e7213df1adfd6c88bd75dcf23d47bd582d83ec4b
Parker Moore <parkrmoore@gmail.com>
2015-01-03 20:32:59 -0800

    Move time parsing into the Serializer.

--- src/gossip/database/message.go
+++ src/gossip/database/message.go
@@ -1,14 +1,10 @@
 package database
 
 import (
+	"gossip/serializer"
+
 	"fmt"
 	"html"
-	"time"
-)
-
-const (
-	HowImUsedToItBeing   = "2006-01-02 15:04:05 -0700"
-	HowJavaScriptSendsIt = "Mon, 02 Jan 2006 15:04:05 MST"
 )
 
 type Message struct {
@@ -29,16 +25,10 @@ func (msg *Message) String() string {
 }
 
 func (msg *Message) ForInsertion() map[string]interface{} {
-	time, err := time.Parse(HowJavaScriptSendsIt, msg.At)
-	if err != nil {
-		fmt.Println(err)
-		return map[string]interface{}{}
-	}
-
 	return map[string]interface{}{
 		"room":    html.EscapeString(msg.Room),
 		"author":  html.EscapeString(msg.Author),
 		"message": html.EscapeString(msg.Message),
-		"at":      time,
+		"at":      serializer.ParseJavaScriptTime(msg.At),
 	}
 }
