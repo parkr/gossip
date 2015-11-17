d1d7afb15a27d4960f2deb45020867e96bfe3d84
Parker Moore <parkrmoore@gmail.com>
2015-11-16 16:19:35 -0800

    auth: panic on no auth token in env

--- auth.go
+++ auth.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"github.com/martini-contrib/auth"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/martini-contrib/auth"
 )
 
+var authToken = os.Getenv("GOSSIP_AUTH_TOKEN")
+
+func getAuthToken() string {
+	if authToken != "" {
+		return authToken
+	}
+	panic("GOSSIP_AUTH_TOKEN is a required environment variable")
+}
+
 func TokenAuthHandler(h http.Handler) http.Handler {
-	token := os.Getenv("GOSSIP_AUTH_TOKEN")
+	token := getAuthToken()
 	handler := func(res http.ResponseWriter, req *http.Request) {
 		err := req.ParseForm()
 		if err != nil {
-			log.Fatal("Error processing the form!!!")
-			log.Fatal(err)
+			log.Println("Error processing the form:", err)
 		}
 		givenToken := req.Form.Get("access_token")
 		if req.URL.Path != "/" && !auth.SecureCompare(givenToken, token) {
